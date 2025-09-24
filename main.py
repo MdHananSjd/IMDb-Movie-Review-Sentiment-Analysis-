cbef9a502a3682e14415a5a7cd098e3c7cf97026
Md. Hanan Sajid <mdhanansjd+github@gmail.com>
2025-09-24 20:20:03 +0530

    Initial push

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import streamlit as st
 import pandas as pd
-import numpy as np
+import re
 import matplotlib.pyplot as plt
 import seaborn as sns
-import re
-import seaborn as sns
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.svm import SVM
